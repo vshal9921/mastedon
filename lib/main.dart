4728ca2ca3f5319c0e74340b73497ab53c754a81
Vishal Gupta <vshal9921@gmail.com>
2024-06-09 20:51:15 +0530

    init register screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'ui/register.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text('Mastedon'),
+      home: Register(),
     );
   }
 }
